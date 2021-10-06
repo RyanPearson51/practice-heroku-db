f6d9d4941b84edf6f8050612a779465ee32fb43a
Ryan Pearson <ryanpearson015@gmail.com>
2021-10-05 20:00:14 -0500

    commiting intial code

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+require('dotenv').config();
+
+let app = express();
+
+let PORT = process.env.PORT;
+
+app.get('/', function(req, res){
+    res.send('hello heroku app');
+})
+
+app.listen(PORT, function(){
+    console.log('starting application on port', PORT)
+})
